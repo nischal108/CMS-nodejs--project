f20238df8e536ad9911cccffaf3ca765ad2ba868
Nischal Bhattarai <nishchalb21@gmail.com>
2023-09-06 10:21:44 +0545

    form handling

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express()
 
 app.set("view engine","ejs")
 
+app.use(express.json())
+
+app.use(express.urlencoded({extended:true}))
 
 
 app.get("/",(req,res)=>{
@@ -14,6 +17,12 @@ app.get("/create",(req,res)=>{
     res.render("create.ejs")
 })
 
+
+//api to post blog
+
+app.post("/create", (req,res) =>{
+    console.log(req.body)
+})
 
 
 app.listen(3000,function(){
